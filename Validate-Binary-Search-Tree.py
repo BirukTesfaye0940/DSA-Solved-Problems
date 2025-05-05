71c156eec065e32b65f5c73255c4b34b8a0fd0c4
Yonas Zebrie <yonaszebrie@Biruks-MacBook-Pro.local>
2025-05-05 11:51:30 +0300

    Invert Binary tree solution

--- Validate-Binary-Search-Tree.py
+++ Validate-Binary-Search-Tree.py
@@ -1,12 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def vlaid (node, left, right):
+    def isValidBST(self, root: TreeNode) -> bool:
+        def valid (node, left, right):
             if not node:
                 return True
             elif not (node.val > left and node.val < right):
